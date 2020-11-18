53e9e44eb0dde9e5c75d9929bc04a4d71ac8d660
João Ferreira <2171315@my.ipleiria.pt>
2020-11-18 12:08:36 +0000

    Initial commit + home page
    Initial commit com a homepage pois não tinha feito repo nem pensado em fazer um visto ser o único a trabalhar no projeto

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <nav id="nav" class="navbar navbar-expand-lg navbar-dark my-dark mt-3">
+      <a class="navbar-brand ml-4 font-weight-bold" href="#">João Ferreira</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+          <!--    mr-auto para que o próximo vá para a direita.      -->
+        </ul>
+        <ul class="navbar-nav mr-5">
+          <li class="nav-item mr-2">
+            <router-link class="nav-link text-white" to="/">
+              <p class="d-inline small font-weight-bold text-info mr-2">00. </p>
+              Início
+            </router-link>
+          </li>
+          <li class="nav-item mr-2">
+            <router-link class="nav-link text-white" to="/about">
+              <p class="d-inline small font-weight-bold text-info mr-2">01. </p>
+              Sobre mim
+            </router-link>
+          </li>
+          <li class="nav-item mr-2">
+            <router-link class="nav-link text-white" to="/about">
+              <p class="d-inline small font-weight-bold text-info mr-2">02. </p>
+              Experiência
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/about">
+              <p class="d-inline small font-weight-bold text-info mr-2">03. </p>
+              Tecnologias
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+    @import './assets/styles/mycss.css';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="centerPseudo">
+    <div class="info">
+      <h1 class="text-white apresentacao ml-5">Olá, sou o João.</h1>
+      <p class="myinfo">Sou um developer, natural das Caldas da Rainha.</p>
+<!--      <p class="myinfo">A minha área de maior interesse neste momento é web mas gosto de tudo um pouco!</p>-->
+    </div>
+    <img class="photo" src="/imgs/eu.jpg" alt="Uma foto minha">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+
+.centerPseudo {
+  width: 60%;
+  height: 60%;
+  position: absolute;
+  top:0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+
+.info {
+  width: 60%;
+  float: left;
+}
+
+.photo {
+  width: 35%;
+  float: left;
+  margin-left: 5%;
+  background: black;
+}
+
+.apresentacao{
+  text-align: left;
+  font-size: 350%;
+}
+
+.myinfo{
+  margin-top: 5%;
+  text-align: left;
+  font-size: 150%;
+  color: lightgray;
+}
+
+
+</style>
