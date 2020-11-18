a9df1612f6d1d13de7e4b0e5662942d5dfa19efa
João Ferreira <2171315@my.ipleiria.pt>
2020-11-18 12:51:21 +0000

    about me page
    Inicio da página sobre mim e remoção do style na página home para um ficheiro css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="sobre-mim">
+      <h1 class="text-white titulo-about">Sobre mim</h1>
+      <p class="text-white descricao-about mt-5">Descobre aqui quem sou, de onde sou e por onde andei</p>
+    </div>
+    <div class="box">
+
+    </div>
   </div>
 </template>
+
+<style>
+.sobre-mim{
+  height: 200px;
+  text-align: center;
+}
+
+.titulo-about{
+  margin-top: 5%;
+  font-size: 350%;
+}
+
+.descricao-about{
+  font-size: 150%;
+}
+
+
+.box:after {
+  height: 70%;
+  content: ' ';
+  border-top: 10vh solid #191a1d;
+  border-left: 100vw solid white;
+  position: absolute;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="centerPseudo">
     <div class="info">
       <h1 class="text-white apresentacao ml-5">Olá, sou o João.</h1>
-      <p class="myinfo">Sou um developer, natural das Caldas da Rainha.</p>
+      <p class="myinfo">Faço uns sites e programo umas outras coisas</p>
 <!--      <p class="myinfo">A minha área de maior interesse neste momento é web mas gosto de tudo um pouco!</p>-->
     </div>
     <img class="photo" src="/imgs/eu.jpg" alt="Uma foto minha">
@@ -21,42 +21,3 @@ export default {
 }
 </script>
 
-<style>
-
-.centerPseudo {
-  width: 60%;
-  height: 60%;
-  position: absolute;
-  top:0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-
-.info {
-  width: 60%;
-  float: left;
-}
-
-.photo {
-  width: 35%;
-  float: left;
-  margin-left: 5%;
-  background: black;
-}
-
-.apresentacao{
-  text-align: left;
-  font-size: 350%;
-}
-
-.myinfo{
-  margin-top: 5%;
-  text-align: left;
-  font-size: 150%;
-  color: lightgray;
-}
-
-
-</style>
