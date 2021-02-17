04aa57215f3a59d5506b13f49d15e455582a4011
João Ferreira <2171315@my.ipleiria.pt>
2021-02-17 17:02:33 +0000

    pagina inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="centerPseudo">
-    <div class="info">
-      <h1 class="text-white apresentacao ml-5">Olá, sou o João.</h1>
-      <p class="myinfo">Sou um developer, natural das Caldas da Rainha.</p>
-<!--      <p class="myinfo">A minha área de maior interesse neste momento é web mas gosto de tudo um pouco!</p>-->
-    </div>
-    <img class="photo" src="/imgs/eu.jpg" alt="Uma foto minha">
-  </div>
+	<!--	<div class="centerPseudo">-->
+	<!--		<div class="info">-->
+	<!--			<h1 class="text-white apresentacao ml-5">Olá, sou o João.</h1>-->
+	<!--			<p class="myinfo">Um programador natural das Caldas da Rainha.</p>-->
+	<!--&lt;!&ndash;			<p class="myinfo">A minha área de maior interesse neste momento é web mas gosto de tudo um pouco!</p>&ndash;&gt;-->
+	<!--		</div>-->
+	<!--		<div class="infoPhoto">-->
+	<!--			<img class="photo" src="/imgs/eu.jpg" alt="Uma foto minha">-->
+	<!--		</div>-->
+	<!--	</div>-->
+
+	<div>
+		<div class="d-flex justify-content-center mt-5">
+			<!-- source https://stackoverflow.com/questions/45666656/bootstrap-4-responsive-utilities-visible-hidden-xs-sm-lg-not-working -->
+			<!--	Computer	-->
+			<div class="row d-none d-lg-flex d-xl-none d-xl-flex">
+				<div class="col-xs-6 text-center text-justify text-white">
+					<h1 class="display-4">Olá, sou o João.</h1>
+					<p class="mt-2 h3">Um programador natural das Caldas da Rainha.</p>
+				</div>
+				<div class="col-xs-6 text-center ml-5">
+					<img src="/imgs/eu.jpg" class="photo" alt="Uma foto minha">
+				</div>
+			</div>
+			<!--	Mobile    -->
+			<div class="row d-block d-sm-block d-md-block d-lg-none">
+				<div class="col-xs-6 text-center text-white">
+					<h1 class="display-4">Olá, sou o João.</h1>
+					<p class="mt-2 h3">Um programador natural das Caldas da Rainha.</p>
+				</div>
+				<div class="col-xs-6 text-center">
+					<img src="/imgs/eu.jpg" class="photo" alt="Uma foto minha">
+				</div>
+			</div>
+		</div>
+	</div>
+
 </template>
 
 <script>
@@ -14,49 +43,17 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    // HelloWorld
-  }
+	name: 'Home',
+	components: {
+		// HelloWorld
+	}
 }
 </script>
 
 <style>
 
-.centerPseudo {
-  width: 60%;
-  height: 60%;
-  position: absolute;
-  top:0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-}
-
-.info {
-  width: 60%;
-  float: left;
-}
-
 .photo {
-  width: 35%;
-  float: left;
-  margin-left: 5%;
-  background: black;
+	width: 340px;
 }
-
-.apresentacao{
-  text-align: left;
-  font-size: 350%;
-}
-
-.myinfo{
-  margin-top: 5%;
-  text-align: left;
-  font-size: 150%;
-  color: lightgray;
-}
-
 
 </style>
